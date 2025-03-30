64a800edd1153646ddeb8ebc17be560f84dd1521
Yoji Takeuchi <takeuchi@e-trees.jp>
2025-03-30 15:18:14 +0900

    feat: Rewrite with VOLK

--- lib/calcPhaseDiff_impl.cc
+++ lib/calcPhaseDiff_impl.cc
@@ -8,6 +8,7 @@
 #include "calcPhaseDiff_impl.h"
 #include <gnuradio/io_signature.h>
 #include <gnuradio/math.h>
+#include <volk/volk.h>
 
 namespace gr {
 namespace etreesModule {
@@ -46,6 +47,7 @@ int calcPhaseDiff_impl::work(int noutput_items,
     auto out = static_cast<output_type*>(output_items[0]);
 
     // Do <+signal processing+>
+    /*
     float in0_re, in0_im;
     float in1_re, in1_im;
     float mult0, mult1, mult2, mult3;
@@ -66,6 +68,61 @@ int calcPhaseDiff_impl::work(int noutput_items,
 	add = mult2 + mult3;
 	out[index] = atan2f(sub, add);
     }
+    */
+
+    float *in0_re, *in0_im;
+    float *in1_re, *in1_im;
+    float *mult0, *mult1, *mult2, *mult3;
+    float *sub;
+    float *add;
+
+    gr_complex *c;
+    
+    in0_re = (float *)volk_malloc(noutput_items * sizeof(float), volk_get_alignment());
+    in0_im = (float *)volk_malloc(noutput_items * sizeof(float), volk_get_alignment());
+    in1_re = (float *)volk_malloc(noutput_items * sizeof(float), volk_get_alignment());
+    in1_im = (float *)volk_malloc(noutput_items * sizeof(float), volk_get_alignment());
+
+    mult0 = (float *)volk_malloc(noutput_items * sizeof(float), volk_get_alignment());
+    mult1 = (float *)volk_malloc(noutput_items * sizeof(float), volk_get_alignment());
+    mult2 = (float *)volk_malloc(noutput_items * sizeof(float), volk_get_alignment());
+    mult3 = (float *)volk_malloc(noutput_items * sizeof(float), volk_get_alignment());
+
+    add = (float *)volk_malloc(noutput_items * sizeof(float), volk_get_alignment());
+    sub = (float *)volk_malloc(noutput_items * sizeof(float), volk_get_alignment());
+
+    c = (gr_complex *)volk_malloc(noutput_items * sizeof(gr_complex), volk_get_alignment());
+
+    volk_32fc_deinterleave_32f_x2(in0_re, in0_im, in0, noutput_items);
+    volk_32fc_deinterleave_32f_x2(in1_re, in1_im, in1, noutput_items);
+
+    volk_32f_x2_multiply_32f(mult0, in0_re, in1_im, noutput_items);
+    volk_32f_x2_multiply_32f(mult1, in1_re, in0_im, noutput_items);
+    volk_32f_x2_multiply_32f(mult2, in0_re, in1_re, noutput_items);
+    volk_32f_x2_multiply_32f(mult3, in0_im, in1_im, noutput_items);
+
+    volk_32f_x2_add_32f(add, mult2, mult3, noutput_items);
+    volk_32f_x2_subtract_32f(sub, mult0, mult1, noutput_items);
+
+    volk_32f_x2_interleave_32fc(c, add, sub, noutput_items);
+
+    for (int index = 0; index < noutput_items; index++) {
+        //    out[i] = std::arg (in[i]);                                                                                                    
+        out[index] = gr::fast_atan2f(c[index]);
+    }
+    
+    volk_free(in0_re);
+    volk_free(in0_im);
+    volk_free(in1_re);
+    volk_free(in1_im);
+
+    volk_free(mult0);
+    volk_free(mult1);
+    volk_free(mult2);
+    volk_free(mult3);
+
+    volk_free(add);
+    volk_free(sub);
 
     // Tell runtime system how many output items we produced.
     return noutput_items;
